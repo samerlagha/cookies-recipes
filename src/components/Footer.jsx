1bbeb91a2510016060522617ceac2bd69ece3428
Your Name <samerukr@gmail.com>
2021-03-12 17:50:57 +0200

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 function Footer() {
     return (
-        <footer className='page-footer green lighten-4'>
+        <footer className='page-footer blue lighten-2'>
             <div className='footer-copyright'>
                 <div className='container'>
                     © {new Date().getFullYear()} Copyright Text
                     <a
                         className='grey-text text-lighten-4 right'
-                        href='https://github.com/michey85/react-food'
+                        href='https://github.com/samerlagha/cookies-recipes'
                         rel='noreferrer'
                         target='_blank'
                     >
